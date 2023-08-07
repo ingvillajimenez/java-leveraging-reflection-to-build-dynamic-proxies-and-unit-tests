5d7238383a562ca4c13dfb5a1ce001eaea2f0ce1
Juan Villa <ing.villa.jimenez@gmail.com>
2023-08-06 23:07:55 -0600

    Invoking Interface and Object Functions on Proxies

--- SkillsoftJavaReflection/src/com/skillsoft/reflection/Main.java
+++ SkillsoftJavaReflection/src/com/skillsoft/reflection/Main.java
@@ -25,6 +25,43 @@ public class Main {
         //Method invoked: public abstract java.lang.Object com.skillsoft.reflection.Repository.read(int)
         //Arguments: [12345]
 
+        System.out.println();
+
+        proxyRepository.update("World");
+        //Proxy: class com.sun.proxy.$Proxy0
+        //Method invoked: public abstract void com.skillsoft.reflection.Repository.update(java.lang.Object)
+        //Arguments: [World]
+
+        System.out.println();
+
+        proxyRepository.delete(67890);
+        //Proxy: class com.sun.proxy.$Proxy0
+        //Method invoked: public abstract void com.skillsoft.reflection.Repository.delete(int)
+        //Arguments: [67890]
+
+        System.out.println();
+
+        proxyRepository.toString(); // Result of 'Object.toString()' is ignored
+        //Proxy: class com.sun.proxy.$Proxy0
+        //Method invoked: public java.lang.String java.lang.Object.toString()
+        //Arguments: null
+
+        System.out.println();
+
+        System.out.println(proxyRepository);
+        //Proxy: class com.sun.proxy.$Proxy0
+        //Method invoked: public java.lang.String java.lang.Object.toString()
+        //Arguments: null
+        //--------------
+        //Any return value is fine here
+
+        System.out.println();
+
+        proxyRepository.hashCode(); // Result of 'Object.hashCode()' is ignored
+        //Proxy: class com.sun.proxy.$Proxy0
+        //Method invoked: public native int java.lang.Object.hashCode()
+        //Arguments: null
+        //java.lang.ClassCastException: class java.lang.String cannot be cast to class java.lang.Integer
     }
 }
 
--- SkillsoftJavaReflection/src/com/skillsoft/reflection/SimplePrintInvocationHandler.java
+++ SkillsoftJavaReflection/src/com/skillsoft/reflection/SimplePrintInvocationHandler.java
@@ -13,15 +13,28 @@ public class SimplePrintInvocationHandler implements InvocationHandler {
         System.out.println("Proxy: " + proxy.getClass());
         // class com.sun.proxy.$Proxy0
         // class com.sun.proxy.$Proxy0
+        // class com.sun.proxy.$Proxy0
+        // class com.sun.proxy.$Proxy0
+        // class com.sun.proxy.$Proxy0
+        // class com.sun.proxy.$Proxy0
         System.out.println("Method invoked: " + method);
         // public abstract void com.skillsoft.reflection.Repository.create(java.lang.Object)
         // public abstract java.lang.Object com.skillsoft.reflection.Repository.read(int)
+        // public abstract void com.skillsoft.reflection.Repository.update(java.lang.Object)
+        // public abstract void com.skillsoft.reflection.Repository.delete(int)
+        // public java.lang.String java.lang.Object.toString()
+        // public native int java.lang.Object.hashCode()
         System.out.println("Arguments: " + Arrays.toString(args));
         // [Hello]
         // [12345]
+        // [World]
+        // [67890]
+        // null
+        // null
 
         System.out.println("--------------");
 
-        return "Any return value is fine here";
+        return true; //Incompatible type might be returned when proxying method 'hashCode()': required: exactly Integer; got: exactly Boolean
+//        return "Any return value is fine here";
     }
 }
